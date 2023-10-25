cd8d8eb20e333243c2b421368b69c0b5fec4446b
RaY8118 <parthghadi43@gmail.com>
2023-10-25 21:06:01 +0530

    automatic attendance

--- Generator/generator_keys.py
+++ Generator/generator_keys.py
@@ -3,12 +3,12 @@ from pathlib import Path
 
 import streamlit_authenticator as stauth
 
-names = ["admin1", "admin2", "admin3", "student1"]
-usernames = ["admin1", "admin2", "admin3", "student1"]
-passwords = ["admin123", "admin213", "admin321", "stu1"]
+names = ["admin1", "admin2","student1"]
+usernames = ["admin1", "admin2", "student1"]
+passwords = ["admin123", "admin213", "stu1"]
 
 hashed_passwords = stauth.Hasher(passwords).generate()
-file_path = Path(__file__).parent / "hashed_pw.pkl"
+file_path = Path(__file__).parent / "../Resources/hashed_pw.pkl"
 
 with file_path.open("wb") as file:
     pickle.dump(hashed_passwords, file)
